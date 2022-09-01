28309e1934bcdeb702e08891ce9bb571351d435c
Jiju  Selvakumar <Jiju.S@avasoft.com>
2022-09-01 16:51:56 +0530

    Configure service methods

--- src/router/AttendanceRouter.ts
+++ src/router/AttendanceRouter.ts
@@ -2,47 +2,31 @@ import express, { Request, Response } from "express";
 import { Rollbar } from "../helpers/Rollbar";
 import Attendance from "../models/Attendance.model";
 import AttendanceDb from "../schema/AttendanceSchema";
+import {
+  getAttendanceForParticularDates,
+  getAttendanceStatus,
+} from "../services/AttendenceSevice";
 
 const router = express.Router();
 
 router.get("/", async (req: Request, res: Response) => {
   try {
     if (req.query["userId"] && req.query["date"]) {
-      const parsedDate = new Date(req.query["date"] as string);
-
-      let query = {
-        microsoftUserID: req.query["userId"],
-        date: new Date(
-          new Date(
-            parsedDate.getFullYear(),
-            parsedDate.getMonth(),
-            parsedDate.getDate()
-          ).setHours(0, 0, 0, 0)
-        ),
-      };
-
-      let attendance: Attendance[] = await AttendanceDb.find(query);
-
-      var status =
-        attendance.length > 0 ? attendance[0].attendance_status : "Not Filled";
+      const status = await getAttendanceStatus(
+        req.query["date"] as string,
+        req.query["userId"] as string
+      );
 
       return res.status(200).json(status);
     }
 
     if (req.query["userId"] && req.query["fromDate"] && req.query["toDate"]) {
-      const query = {
-        $and: [
-          { $gte: { date: req.query["fromDate"] as string } },
-          { $lte: { date: req.query["toDate"] as string } },
-          { microsoftUserID: req.query["userId"] as string },
-        ],
-      };
-      let result = await AttendanceDb.find(query);
-      var attendanceArray = JSON.parse(
-        JSON.stringify(result)
-          .replace(new RegExp("_id", "g"), "id")
-          .replace(new RegExp("__v", "g"), "v")
+      const attendanceArray = await getAttendanceForParticularDates(
+        req.query["userId"] as string,
+        req.query["fromDate"] as string,
+        req.query["toDate"] as string
       );
+      
       return res.status(200).json(attendanceArray);
     }
 
